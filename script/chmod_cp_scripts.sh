f6ed847471daec49bf253ca983f2019917d54be4
mro66 <dr.m.roehrig@googlemail.com>
2021-06-23 10:12:37 +0200

    Änderungen an Shell Skripten

--- script/chmod_cp_scripts.sh
+++ script/chmod_cp_scripts.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-sudo cp ./picontrol.sh /usr/local/sbin/
+sudo chmod -R 777 ./picontrol
+sudo cp ./picontrol /usr/local/sbin/
 sudo chmod -R 777 ../tripmaster_web.py
 sudo chmod -R 777 ./*.sh
