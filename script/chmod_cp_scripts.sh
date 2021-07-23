b016eabbbb71e8d8585d3be4fc97e0c8f464ecc3
mro66 <dr.m.roehrig@googlemail.com>
2021-07-23 19:22:01 +0200

    Installationsskripte

--- script/chmod_cp_scripts.sh
+++ script/chmod_cp_scripts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo chmod -R 777 ./picontrol
-sudo cp ./picontrol /usr/local/sbin/
+sudo chmod -R 777 ./tmcontrol
+sudo cp ./picontrol /usr/bin/
 sudo chmod -R 777 ../tripmaster_web.py
 sudo chmod -R 777 ./*.sh
