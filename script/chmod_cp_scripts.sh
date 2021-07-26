5fa29262b7a590db5551efbf310fe4f9796ac006
mro66 <dr.m.roehrig@googlemail.com>
2021-07-26 18:06:13 +0200

    Kontrollskript

--- script/chmod_cp_scripts.sh
+++ script/chmod_cp_scripts.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo chmod -R 777 ./tmcontrol
-sudo cp ./picontrol /usr/bin/
+sudo cp ./tmcontrol /usr/bin/
 sudo chmod -R 777 ../tripmaster_web.py
+sudo chmod -R 777 ../gps_test.py
 sudo chmod -R 777 ./*.sh
